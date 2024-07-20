822afe7920514cb37ca583163bb34e52d8d03253
josePc <jose.csousa22@gmail.com>
2024-07-20 14:26:59 -0300

    chore: Remove teste do CadastrarUsuarioDtoValidator

--- Application/Application/Authorization/Dto/Validators/CadastrarUsuarioDtoValidator.cs
+++ Application/Application/Authorization/Dto/Validators/CadastrarUsuarioDtoValidator.cs
@@ -10,18 +10,12 @@ public class CadastrarUsuarioDtoValidator : AbstractValidator<CadastrarUsuarioDt
     {
         RuleFor(request => request.Email)
             .NotEmpty()
-            .WithMessage(GetErrorMessage(ResourceErrorMessage.EMAIL_VAZIO))
-            .Must(email => Regex.IsMatch(email, "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$"))
-            .WithMessage(GetErrorMessage(ResourceErrorMessage.FORMATO_EMAIL_INVALIDO));
+            .WithMessage(ResourceErrorMessage.EMAIL_VAZIO)
+            .EmailAddress()
+            .WithMessage(ResourceErrorMessage.FORMATO_EMAIL_INVALIDO);
 
         RuleFor(request => request.Password)
             .NotEmpty()
-            .WithMessage(GetErrorMessage(ResourceErrorMessage.SENHA_VAZIA));
-    }
-
-    private string GetErrorMessage(string resourceMessage)
-    {
-        Console.WriteLine($"Loading error message: {resourceMessage}");
-        return resourceMessage ?? "Default error message";
+            .WithMessage(ResourceErrorMessage.SENHA_VAZIA);
     }
 }
